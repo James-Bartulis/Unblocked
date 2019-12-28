c75c7c7b564e9c3a72ed362a7f188b5e03065229
James Bartulis <jbart012@ucr.edu>
2019-12-27 23:49:05 -0800

    update fontsize

--- main.cpp
+++ main.cpp
@@ -5,8 +5,21 @@
 
 using namespace std;
 
+void SetFontSize(int size){
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = 0;                   // Width of each character in the font
+	cfi.dwFontSize.Y = size;                  // Height
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+	std::wcscpy(cfi.FaceName, L"Consolas"); // Choose your font
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+}
+
 int main(){
 
+	SetFontSize(36);
 	Game* test = new Game();
 
 	while(test->isGameRunning()){
@@ -14,5 +27,5 @@ int main(){
 		test->Input();
 		test->Logic();
 	}
-
+	SetFontSize(16);
 }
